c2d5fbae71973779d80135c2e2e42366dc345d10
ruying <164972038@qq.com>
2019-03-12 21:23:45 +0800

    increase front and back ends about order page

--- dailyfresh/apps/order/views.py
+++ dailyfresh/apps/order/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.views.generic import View
+from django.http import JsonResponse
 
 from django_redis import get_redis_connection
 
 from goods.models import GoodsSKU
 from user.models import Address
+from order.models import OrderInfo, OrderGoods
+
+from datetime import datetime
+
 from utils.mixin import LoginRequiredMixin
 # Create your views here.
 
@@ -65,3 +70,93 @@ class OrderPlaceView(LoginRequiredMixin, View):
         }
         # 返回应答
         return render(request, 'place_order.html', context)
+
+
+# 前端传递的参数：地址id(addr_id), 支付方式(pay_method),用户要购买的商品id字符串(sku_ids)
+class OrderCommitView(View):
+    '''订单创建'''
+    def post(self,request):
+        '''订单创建'''
+        # 判断用户是否登录
+        user = request.user
+        if not user.is_authenticated():
+            # 用户未登录
+            return JsonResponse({'res':0, 'errmsg': '用户未登录'})
+
+        # 接收参数
+        addr_id = request.POST.get('addr_id')
+        pay_method = request.POST.get('pay_method')
+        sku_ids = request.POST.get('sku_ids')
+
+        # 校验参数
+        if not all([addr_id, pay_method, sku_ids]):
+            return JsonResponse({'res': 1, 'errmsg': '参数不完整'})
+
+        # 校验支付方式
+        if pay_method not in OrderInfo.PAY_METHOD.keys():
+            return JsonResponse({'res': 2, 'errmsg': '非法的支付方式'})
+
+        # 校验地址
+        try:
+            addr = Address.objects.get(id= addr_id)
+        except Address.DoesNotExist:
+            return JsonResponse({'res': 3, 'errmsg':'地址不正确'})
+
+        # todo创建订单核心业务
+
+        # 组织参数
+        # 订单id
+        order_id = datetime.now().strftime('%Y%m%d%H%M%S')+str(user.id)
+
+        # 运费
+        transit_price = 10
+
+        # 总数目和总金额
+        total_count =0
+        total_price =0
+
+        # todo: 向order_info添加一条记录
+        order = OrderInfo.objects.create(order_id=order_id,
+                                         user=user,
+                                         addr=addr,
+                                         pay_method=pay_method,
+                                         total_count=total_count,
+                                         total_price=total_price,
+                                         transit_price=transit_price)
+        # todo: 用户订单中有几个商品，向OrderGoods中添加记录
+        conn = get_redis_connection('defalut')
+        cart_key = 'cart_%d'%user.id
+        sku_ids = sku_ids.split(',')
+        for sku_id in sku_ids:
+            try:
+                sku = GoodsSKU.objects.get(id=sku_id)
+            except GoodsSKU.DoesNotExist:
+                return JsonResponse({'res': 4, 'errmsg': '商品不存在'})
+            count = conn.hget(cart_key, sku_id)
+            price = sku.price
+
+            # todo:添加记录
+            OrderGoods.objects.create(order=order,
+                                      sku=sku,
+                                      count=count,
+                                      price=price)
+
+            # todo: 更新商品的库存和销量
+            sku.stock -= 1
+            sku.sales += 1
+            sku.save()
+
+            # todo: 计算订单商品的总金额和总数量
+            amount = sku.price*int(count)
+            total_price += amount
+            total_count += int(count)
+
+        # todo: 更新订单信息表中的商品总数量和总价格
+        order.total_count = total_count
+        order.total_price = total_price
+        order.save()
+
+        # todo: 清除用户购物车中对应的记录
+        conn.hdel(cart_key, *sku_ids)
+
+        return JsonResponse({'res':5, 'message': '创建成功'})
